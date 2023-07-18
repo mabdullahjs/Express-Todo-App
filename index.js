547bc373c694228a62058c0a6a5273f9e411dba0
mabdullahjs <mabdullah2037@outlook.com>
2023-07-19 00:00:45 +0500

    middleware changed

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-require("dotenv").config();
 
 // imports
 const express = require("express");
 const mongoose = require("mongoose");
 const courseRoutes = require("./routes/course");
+var cors = require('cors')
+require("dotenv").config();
 
 //express app
 const app = express();
 
 //cors for production
-var cors = require('cors')
 app.use(cors()) 
 
 //middleware
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
-
 
 // routes
 app.use("/course" , courseRoutes);
@@ -27,7 +21,9 @@ app.use("/course" , courseRoutes);
 
 
 
-
+app.get("/", (req, res) => {
+  res.send("Server Started");
+});
 
 // connect database
 mongoose
@@ -35,7 +31,7 @@ mongoose
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(
-        "Database Connected Successfully and server is listening on this port 5000"
+        `Database Connected Successfully and server is listening on this port ${process.env.PORT}`
       );
     });
   })
